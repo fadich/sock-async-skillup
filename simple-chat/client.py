359317fc5c363b3f555837865bcf28d965542af5
Fadi Ahmad (Kharkiv) <f.ahmad@mobidev.biz>
2018-06-15 16:19:49 +0300

    client

--- simple-chat/client.py
+++ simple-chat/client.py
@@ -23,15 +23,16 @@ logger = log.get_logger('Client')
 
 class AsyncThread(threading.Thread):
     loop: asyncio.BaseEventLoop = None
+    task: asyncio.Task = None
 
     def __init__(self, *args, **kwargs):
         self.loop = asyncio.new_event_loop()
 
         super().__init__(*args, **kwargs)
 
-        task = self.loop.create_task(
+        self.task = self.loop.create_task(
             self._target(*self._args, **self._kwargs), )
-        wait_tasks = asyncio.wait([task])
+        wait_tasks = asyncio.wait([self.task])
 
         self._target = self.loop.run_until_complete
         self._args = (wait_tasks, )
@@ -39,13 +40,11 @@ class AsyncThread(threading.Thread):
 
 class Client(object):
     host: str = ''
-    loop: asyncio.BaseEventLoop = None
     session: aiohttp.ClientSession = None
     ws: aiohttp.ClientWebSocketResponse = None
 
     def __init__(self, host: str):
         self.host = host
-        self.loop = asyncio.new_event_loop()
 
     async def connect(self, handle_msg: callable, on_connected: callable = None,
                       autoreconnect: bool = True):
@@ -60,9 +59,14 @@ class Client(object):
                 if on_connected:
                     on_connected(self.session, self.ws)
 
-                async for msg in self.ws:
-                    await asyncio.sleep(0)
-                    handle_msg(msg)
+                while True:
+                    try:
+                        msg = await self.ws.receive(timeout=0.01)
+                        handle_msg(msg)
+                    except asyncio.TimeoutError:
+                        pass
+                    finally:
+                        await asyncio.sleep(0)
 
             except aiohttp.client_exceptions.ClientConnectorError:
                 if not autoreconnect:
@@ -70,17 +74,26 @@ class Client(object):
                 logger.info('Reconnecting...')
                 await asyncio.sleep(1)
 
+            except asyncio.CancelledError:
+                logger.info('Connection aborted.')
+                break
+
             except Exception as e:
                 logger.error(e)
                 logger.warning(traceback.format_exc())
                 break
 
+        await self.close()
+
     async def close(self):
-        if self.session and not self.session.closed:
-            await self.session.close()
 
         if self.ws and not self.ws.closed:
+            logger.debug('Closing ws...')
             await self.ws.close()
+
+        if self.session and not self.session.closed:
+            logger.debug('Closing session...')
+            await self.session.close()
 
     async def send_command(self, command: str, data: Any = None):
         await self.ws.send_json({
@@ -94,7 +107,7 @@ class Window(Gtk.ApplicationWindow):
     grid: Gtk.Grid = None
     client: Client = None
     event_loop: asyncio.BaseEventLoop = None
-    client_thread: threading.Thread = None
+    client_thread: AsyncThread = None
 
     clients = {}
 
@@ -123,7 +136,7 @@ class Window(Gtk.ApplicationWindow):
         self.client_thread = AsyncThread(
             target=self.client.connect,
             args=(self.msg_handler, self.on_connected, ),)
-        client_thread.start()
+        self.client_thread.start()
 
         Gtk.main()
 
@@ -137,9 +150,8 @@ class Window(Gtk.ApplicationWindow):
         #     self.send_msg('Hi!')
 
     def close(self, window):
-        client_thread.stop()
+        self.client_thread.task.cancel()
         # pass
-        # self.client.loop.run_until_complete(self.client.close())
 
         # if self.client_thread and self.client_thread.is_alive():
         #     self.client_thread.join()
@@ -147,21 +159,21 @@ class Window(Gtk.ApplicationWindow):
     def on_connected(self, session: aiohttp.ClientSession,
                      ws: aiohttp.ClientWebSocketResponse):
 
-        # self.send_profile('My name')
-        pass
+        self.send_profile('My name')
+        # pass
 
     def send_msg(self, msg: str):
-        self.client.loop.run_until_complete(
+        self.client_thread.loop.run_until_complete(
             self.client.send_command('msg', msg))
 
     def send_profile(self, name: str):
-        self.client.loop.run_until_complete(
+        self.client_thread.loop.run_until_complete(
             self.client.send_command('profile', {
                 'name': name,
             }))
 
     def send_user_list(self):
-        self.client.loop.run_until_complete(
+        self.client_thread.loop.run_until_complete(
             self.client.send_command('user_list'))
 
     def msg_handler(self, message):
@@ -171,9 +183,6 @@ class Window(Gtk.ApplicationWindow):
         msg = json.loads(message.data)
         command = msg.get('c')
         data = msg.get('d')
-
-        # print('Command: {}'.format(command))
-        # print('Data: {}'.format(data))
 
         if command == 'user_list':
             self.clients = data
